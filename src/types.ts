eb77b70f3c6e22e5ba9c0dd5a67d884af1953cfc
Sidharth Rathi <Xodedeveloper@gmail.com>
2022-06-13 19:35:05 +0530

    fix: improve types

--- src/types.ts
+++ src/types.ts
@@ -3,14 +3,26 @@
  */
 
 import { Sheet2JSONOpts } from "xlsx"
+import type { ZodIssue } from "zod"
 
-export type Result = {}
+type Resource = {
+  issues: ZodIssue[]
+  isValid: boolean
+  data: Record<string, any>
+}
+
+export type Result = {
+  valid: Resource[]
+  invalid: Resource[]
+}
 
 export interface ValidatorOptions {
   header?: Sheet2JSONOpts["header"]
   sheetName?: string
   blankrows?: Sheet2JSONOpts["blankrows"]
   skipHidden?: Sheet2JSONOpts["skipHidden"]
+  onValid?: (data: any) => void
+  onInvalid?: (data: any) => void
 }
 
 /**
